e17b16548a423a2106d426463071e85d14fd375c
keepingoner <keepingoner@163.com>
2019-08-12 22:40:01 +0800

    a

---
+++ com/demo02/main.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+const BLOG  = "http://www.flysnow.org/"
+
+func initStrings(N int) []string{
+	s:=make([]string,N)
+	for i:=0;i<N;i++{
+		s[i]=BLOG
+	}
+	return s
+}
+
+func initStringi(N int) []interface{}{
+	s:=make([]interface{},N)
+	for i:=0;i<N;i++{
+		s[i]=BLOG
+	}
+	return s
+}
+
+
+
+func main() {
+
+	resu := initStringi(2)
+	fmt.Println(resu)
+
+	s := initStrings(4)
+	fmt.Println(s)
+
+}
+*/
